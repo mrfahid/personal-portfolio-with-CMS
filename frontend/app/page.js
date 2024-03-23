7d226376cbe72b907e751193fd1b7c7313796328
Shah Fahid <135055359+mrfahid@users.noreply.github.com>
2024-03-23 16:31:21 +0000

    make navbar

--- frontend/app/page.js
+++ frontend/app/page.js
@@ -1,9 +1,21 @@
-import Image from "next/image";
+// import Image from "next/image";
 import styles from "./page.module.css";
 import './App.scss'
+import {Navbar} from "./components";
+import { About } from "./pages";
+
 
 export default function Home() {
   return (
-    <h1>Next js</h1>
+    <div className="app">
+    <Navbar />
+    <About />
+    {/* <Header />
+    <About />
+    <Work />
+    <Skills />
+    <Testimonial />
+    <Footer /> */}
+  </div>
   );
 }
