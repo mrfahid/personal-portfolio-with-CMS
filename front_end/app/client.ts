97e06d36910fb0daec18cc7fcfb8de62b8ffa2d3
Shah Fahid <135055359+mrfahid@users.noreply.github.com>
2024-05-20 17:53:10 +0000

    ADD API V

--- front_end/app/client.ts
+++ front_end/app/client.ts
@@ -4,11 +4,11 @@ import { SanityImageSource } from '@sanity/image-url/lib/types/types';
 
 // Define your Sanity client configuration
 const clientConfig = {
-  projectId: process.env.NEXT_PUBLIC_SANITY_PROJECT_ID,
-  dataset: process.env.NEXT_PUBLIC_SANITY_DATASET,
-  apiVersion: process.env.NEXT_PUBLIC_SANITY_API_VERSION,
+  projectId: 'k21eb9nx',
+  dataset: 'production',
+  apiVersion: '2024-05-07', // Specify the API version here
   useCdn: false,
-  token:"skPg002lOHSZhI34DybPfBUu4ce1qp3BQZwxqbRuhtVorUD14jcOkUshNhTBcawFqzoNRLjcZs0lxuu3phAYoBAOjE9Sn7fHievfBpMYmwnQAneK2ina8EESVOoxVM5KrztFEhn5nW0H7opJl0wZFhi912zVgLBiREv35X2owhvlxYPLv3RF",
+  token: "skPg002lOHSZhI34DybPfBUu4ce1qp3BQZwxqbRuhtVorUD14jcOkUshNhTBcawFqzoNRLjcZs0lxuu3phAYoBAOjE9Sn7fHievfBpMYmwnQAneK2ina8EESVOoxVM5KrztFEhn5nW0H7opJl0wZFhi912zVgLBiREv35X2owhvlxYPLv3RF"
 };
 
 // Create the Sanity client instance
